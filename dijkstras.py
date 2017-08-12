80f22fe64ee95af80e104e1dcec8b68c298fcc56
Jordan Crabtree <jordan.d.crabtree@gmail.com>
2017-08-12 06:18:00 -0700

    initial commit

---
+++ dijkstras.py
@@ -0,0 +1,28 @@
+import heapq
+from collections import namedtuple, defaultdict
+
+Node = namedtuple('Node', ['depth', 'index'])
+disjoint = float('inf')
+
+def AddAdjacent(heap, adjacency, node):
+    row = node.index
+    for col in xrange(len(adjacency[row])):
+        if adjacency[row][col] == disjoint:
+            continue
+        depth = node.depth + adjacency
+        heapq.heappush(heap, Node(depth=depth, index=col))
+
+def FindShortestDistanceBetween(adjacency, a, b):
+    visited = defaultdict(lambda: False)
+    curr = Node(depth=0, index=a)
+    heap = [curr]
+
+    while curr.index != b and heap:
+        curr = heapq.heappop(heap)
+        if visited[curr.index]:
+            continue
+        visited[curr.index] = True
+        AddAdjacent(heap, adjacency, curr)
+    if curr.index == b:
+        return b.depth
+    return disjoint
---
+++ kruskals.py
@@ -0,0 +1,56 @@
+import heapq
+from collections import namedtuple, defaultdict
+
+
+class Node:
+
+    def __init__(self, index):
+        self.index = index
+        self.parent = None
+        self.rank = 0
+
+    def findParent(self):
+        if self.parent is None:
+            return self
+        self.parent = self.parent.findParent()
+        return self.parent
+
+Edge = namedtuple('Node', ['weight', 'a', 'b'])
+disjoint = float('inf')
+
+
+def AreConnected(nodes, a, b):
+    return nodes[a].findParent().index == nodes[b].findParent().index
+
+
+def Union(nodes, a, b):
+    if AreConnected(nodes, a, b):
+        return
+    lesser = nodes[a].findParent()
+    greater = nodes[b].findParent()
+    if lesser.rank > greater.rank:
+        lesser, greater = greater, lesser
+    lesser.parent = greater
+    if lesser.rank == greater.rank:
+        greater.rank += 1
+
+
+def FindMinimumSpanningTree(adjacent):
+    nodes = map(Node, xrange(len(adjacent)))
+    heap = []
+    result = defaultdict(lambda: defaultdict(lambda: disjoint))
+
+    for i in xrange(len(adjacent)):
+        for j in xrange(len(adjacent[i])):
+            if adjacent[i][j] != disjoint:
+                heapq.heappush(heap, Edge(weight=adjacent[i][j], a=i, b=j))
+
+    while heap:
+        curr = heapq.heappop(heap)
+        a, b = curr.a, curr.b
+        if AreConnected(nodes, a, b):
+            continue
+        Union(nodes, a, b)
+        result[a][b] = curr.weight
+
+    return result
